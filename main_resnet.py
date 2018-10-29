fa76f5d4da26f926a6e388e8fba152bb26965e99
georkap <gkapidis1@gmail.com>
2018-10-29 12:49:16 +0100

    added argument parser

--- main_resnet.py
+++ main_resnet.py
@@ -11,17 +11,19 @@ import os
 import time
 import shutil
 import torch
-from models.resnet18_zoo import resnet101loader
+from models.resnet_zoo import resnet18loader
 import torchvision.transforms as transforms
 import torch.backends.cudnn as cudnn
 from utils.dataset_loader import DatasetLoader, RandomHorizontalFlip, Resize
 from utils.calc_utils import AverageMeter, accuracy
+from utils.argparse_utils import parse_args
+from utils.file_utils import print_and_save
 
-def train(model, optimizer, criterion, device, train_iterator, cur_epoch):
+def train(model, optimizer, criterion, train_iterator, cur_epoch, log_file):
     batch_time, losses, top1, top5 = AverageMeter(), AverageMeter(), AverageMeter(), AverageMeter()
     model.train()
 
-    print('Beginning of epoch: {}'.format(cur_epoch))
+    print_and_save('Beginning of epoch: {}'.format(cur_epoch), log_file)
     t0 = time.time()
     for batch_idx, (inputs, targets) in enumerate(train_iterator):
         inputs = torch.tensor(inputs, requires_grad=True).cuda()
@@ -41,14 +43,14 @@ def train(model, optimizer, criterion, device, train_iterator, cur_epoch):
         losses.update(loss.item(), inputs.size(0))
         batch_time.update(time.time() - t0)
         t0 = time.time()
-        print('[Epoch:{}, Batch {}/{} in {:.3f} s][Loss {:.4f}[avg:{:.4f}], Top1 {:.3f}[avg:{:.3f}], Top5 {:.3f}[avg:{:.3f}]]'.format(
-                cur_epoch, batch_idx, len(train_iterator), batch_time.val, losses.val, losses.avg, top1.val, top1.avg, top5.val, top5.avg))
+        print_and_save('[Epoch:{}, Batch {}/{} in {:.3f} s][Loss {:.4f}[avg:{:.4f}], Top1 {:.3f}[avg:{:.3f}], Top5 {:.3f}[avg:{:.3f}]]'.format(
+                cur_epoch, batch_idx, len(train_iterator), batch_time.val, losses.val, losses.avg, top1.val, top1.avg, top5.val, top5.avg), log_file)
 
-def test(model, device, test_iterator, cur_epoch):
+def test(model, test_iterator, cur_epoch, log_file):
     top1, top5 = AverageMeter(), AverageMeter()
     with torch.no_grad():
         model.eval()
-        print('Evaluating after epoch: {}'.format(cur_epoch))
+        print_and_save('Evaluating after epoch: {}'.format(cur_epoch))
         for batch_idx, (inputs, targets) in enumerate(test_iterator):
             inputs = torch.tensor(inputs).cuda()
             targets = torch.tensor(targets)
@@ -59,52 +61,51 @@ def test(model, device, test_iterator, cur_epoch):
             top1.update(t1.item(), inputs.size(0))
             top5.update(t5.item(), inputs.size(0))
 
-            print('[Epoch:{}, Batch {}/{}][Top1 {:.3f}[avg:{:.3f}], Top5 {:.3f}[avg:{:.3f}]]'.format(
-                    cur_epoch, batch_idx, len(test_iterator), top1.val, top1.avg, top5.val, top5.avg))
+            print_and_save('[Epoch:{}, Batch {}/{}][Top1 {:.3f}[avg:{:.3f}], Top5 {:.3f}[avg:{:.3f}]]'.format(
+                    cur_epoch, batch_idx, len(test_iterator), top1.val, top1.avg, top5.val, top5.avg), log_file)
 
-        print('Test Results: Top1 {:.3f} Top5 {:.3f}'.format(top1.avg, top5.avg))
+        print_and_save('Test Results: Top1 {:.3f} Top5 {:.3f}'.format(top1.avg, top5.avg), log_file)
     return top1.avg
 
 def main():
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    args = parse_args()
     verb_classes = 120
-    max_epochs = 200
-    eval_freq = 1
-    save_freq = 5
-    pretrained = True
-    feature_extraction = False
-    num_workers = 8
-    batch_size = 64
-    output_dir = 'outputs'
-    model_name = 'resnet101_ft_200ep_64bs'
-    output_dir = os.path.join(output_dir, model_name)
+    
+    base_output_dir = args.base_output_dir
+    model_name = args.model_name
+    train_list = args.train_list # r'splits\hand_track_train_1.txt'
+    test_list = args.test_list # r'splits\hand_track_val_1.txt'
+    
+    output_dir = os.path.join(base_output_dir, model_name)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)   
+    if args.logging:
+        log_file = os.path.join(base_output_dir, model_name+".txt")
+    else:
+        log_file = None
 
-    train_list = r'splits\hand_track_train_1.txt'
-    test_list = r'splits\hand_track_val_1.txt'
-
-    model_ft = resnet101loader(verb_classes, pretrained, feature_extraction)
+    model_ft = resnet18loader(verb_classes, args.pretrained, args.feature_extraction) # 120, True, False
     model_ft = torch.nn.DataParallel(model_ft).cuda()
 
     cudnn.benchmark = True
 
     params_to_update = model_ft.parameters()
-    print("Params to learn:")
-    if feature_extraction:
+    print_and_save("Params to learn:", log_file)
+    if args.feature_extraction:
         params_to_update = []
         for name,param in model_ft.named_parameters():
             if param.requires_grad == True:
                 params_to_update.append(param)
-                print("\t",name)
+                print_and_save("\t{}".format(name), log_file)
     else:
         for name,param in model_ft.named_parameters():
             if param.requires_grad == True:
-                print("\t",name)
+                print_and_save("\t{}".format(name), log_file)
 
-    optimizer = torch.optim.SGD(params_to_update, lr=0.001, momentum=0.9)
+    optimizer = torch.optim.SGD(params_to_update,
+                                lr=args.lr, momentum=args.momentum, decay=args.decay)
     ce_loss = torch.nn.CrossEntropyLoss().cuda()
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=70)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_steps[0])
 
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
@@ -112,24 +113,24 @@ def main():
     train_transforms = transforms.Compose([Resize((224,224)), RandomHorizontalFlip(),
                                            transforms.ToTensor(), normalize])
     train_loader = DatasetLoader(train_list, train_transforms)
-    train_iterator = torch.utils.data.DataLoader(train_loader, batch_size=batch_size, num_workers=num_workers, pin_memory=True)
+    train_iterator = torch.utils.data.DataLoader(train_loader, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True)
 
     test_transforms = transforms.Compose([Resize((224,224)),
                                           transforms.ToTensor(), normalize])
     test_loader = DatasetLoader(test_list, test_transforms)
-    test_iterator = torch.utils.data.DataLoader(test_loader, batch_size=batch_size, num_workers=num_workers, pin_memory=True)
+    test_iterator = torch.utils.data.DataLoader(test_loader, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True)
 
     new_top1, top1 = 0.0, 0.0
     isbest = False
-    for epoch in range(max_epochs):
+    for epoch in range(args.max_epochs):
         lr_scheduler.step()
-        train(model_ft, optimizer, ce_loss, device, train_iterator, epoch)
-        if (epoch+1) % eval_freq == 0:
-            new_top1 = test(model_ft, device, test_iterator, epoch)
+        train(model_ft, optimizer, ce_loss, train_iterator, epoch, log_file)
+        if (epoch+1) % args.eval_freq == 0:
+            new_top1 = test(model_ft, test_iterator, epoch, log_file)
             isbest = new_top1 >= top1
-        if (epoch+1) % save_freq == 0:
+        if (epoch+1) % args.save_freq == 0:
             weight_file = os.path.join(output_dir, model_name + '_{:03d}.pth'.format(epoch))
-            print('Saving weights to {}'.format(weight_file))
+            print_and_save('Saving weights to {}'.format(weight_file), log_file)
             torch.save({'epoch': epoch + 1,
                         'state_dict': model_ft.state_dict(),
                         'optimizer': optimizer.state_dict(),
--- utils/argparse_utils.py
+++ utils/argparse_utils.py
@@ -13,24 +13,29 @@ from utils.file_utils import print_and_save
 def parse_args():
     parser = argparse.ArgumentParser(description='Hand activity recognition')
     
+    parser.add_argument('model_name', type=str, help="A name to distinguish the model")
+    parser.add_argument('train_list', type=str)
+    parser.add_argument('test_list', type=str)
+    
     # First load the necessary paths
-    parser.add_argument('--output_dir', type=str, default=r'backup/')
+    parser.add_argument('--base_output_dir', type=str, default=r'outputs/')
     
     # Model parameters
     parser.add_argument('--pretrained', default=False, action='store_true')
-    parser.add_argument('--finetune', default=False, action='store_true')
+    parser.add_argument('--feature_extraction', default=False, action='store_true')
     
     # Training parameters
     parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--lr_steps', nargs='+', type=int, default=[7])
     parser.add_argument('--momentum', type=float, default=0.9)
     parser.add_argument('--decay', type=float, default=0.0005)
-    parser.add_argument('--max_epochs', type=int, default=None)
+    parser.add_argument('--max_epochs', type=int, default=20)
     
     # Program parameters
-    parser.add_argument('--gpus', nargs='+', type=int, default=[0])
-    parser.add_argument('--num_workers', type=int, default=0)
-    parser.add_argument('--eval_freq', type=int, default=2)
+    parser.add_argument('--gpus', nargs='+', type=int, default=[0, 1])
+    parser.add_argument('--num_workers', type=int, default=8)
+    parser.add_argument('--eval_freq', type=int, default=1)
     parser.add_argument('--save_freq', type=int, default=1)
     parser.add_argument('--logging', default=False, action='store_true')
     
