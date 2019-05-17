f27b8b231037a751d2d771c96be7617b0738be0c
georkap <gkapidis1@gmail.com>
2019-05-17 13:17:00 +0200

    Added lr mult to facilitate finetuning in mfnet

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -20,7 +20,56 @@ from torch.optim.lr_scheduler import _LRScheduler
 from utils.calc_utils import AverageMeter, accuracy
 from utils.file_utils import print_and_save
 
-class CyclicLR(_LRScheduler):
+class CustomLRScheduler(object):
+    def __init__(self, optimizer, last_epoch=-1):
+        if not isinstance(optimizer, Optimizer):
+            raise TypeError('{} is not an Optimizer'.format(
+                type(optimizer).__name__))
+        self.optimizer = optimizer
+        if last_epoch == -1:
+            for group in optimizer.param_groups:
+                group.setdefault('initial_lr', group['lr'])
+        else:
+            for i, group in enumerate(optimizer.param_groups):
+                if 'initial_lr' not in group:
+                    raise KeyError("param 'initial_lr' is not specified "
+                                   "in param_groups[{}] when resuming an optimizer".format(i))
+        self.base_lrs = list(map(lambda group: group['initial_lr'], optimizer.param_groups))
+        self.step(last_epoch + 1)
+        self.last_epoch = last_epoch
+
+    def state_dict(self):
+        """Returns the state of the scheduler as a :class:`dict`.
+
+        It contains an entry for every variable in self.__dict__ which
+        is not the optimizer.
+        """
+        return {key: value for key, value in self.__dict__.items() if key != 'optimizer'}
+
+    def load_state_dict(self, state_dict):
+        """Loads the schedulers state.
+
+        Arguments:
+            state_dict (dict): scheduler state. Should be an object returned
+                from a call to :meth:`state_dict`.
+        """
+        self.__dict__.update(state_dict)
+
+    def get_lr(self):
+        raise NotImplementedError
+
+    def step(self, epoch=None):
+        if epoch is None:
+            epoch = self.last_epoch + 1
+        self.last_epoch = epoch
+        for param_group, lr in zip(self.optimizer.param_groups, self.get_lr()):
+            if 'lr_mult' in param_group:
+                lr_mult = param_group['lr_mult']
+            else:
+                lr_mult = 1.0
+            param_group['lr'] = lr * lr_mult # Added lr mult to facilitate finetuning in mfnet
+
+class CyclicLR(CustomLRScheduler):
     """Sets the learning rate of each parameter group according to
     cyclical learning rate policy (CLR). The policy cycles the learning
     rate between two boundaries with a constant frequency, as detailed in
